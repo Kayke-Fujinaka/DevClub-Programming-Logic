9989329f289f865a7494a7afdfb63b3ef7853846
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-04-30 12:24:00 -0300

    Finalizado Módulo de JS

---
+++ JavaScript pt.4 Avançado/Aula 7 - Classes.js
@@ -0,0 +1,19 @@
+class Person {
+
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+
+    talk() {
+    console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old`)
+    }
+}
+
+const newPerson = new Person ("Kayke, 17")
+const newPerson1 = new Person ("Luiza, 37")
+const newPerson2 = new Person ("Vagner, 72")
+
+newPerson.talk()
+newPerson1.talk()
+newPerson2.talk()
